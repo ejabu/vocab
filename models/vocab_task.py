27a76e98d4cf191a16c8b7133fc5d1f68de21d61
ejabu <muhammadfahreza@gmail.com>
2017-01-08 13:32:12 +0700

    Button call wizard

--- models/vocab_task.py
+++ models/vocab_task.py
@@ -18,3 +18,17 @@ class vocab_task(osv.osv):
         'state' : fields.selection([('open', 'Open'), ('done', 'Done')], string='Status'),
 
     }
+    def take_quiz(self, cr, uid, ids, context=None):
+        # self.write(cr, uid, ids, {'state': 'close'}, context=context)
+        # return
+        dummy, view_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'account', 'view_account_chart') #in model name replace . with _ Example sale.order become sale_order
+        import ipdb; ipdb.set_trace()
+        return {
+            'name':"HEHE",#Name You want to display on wizard
+            'view_mode': 'form',
+            'view_id': view_id,
+            'view_type': 'form',
+            'res_model': 'account.chart',# With . Example sale.order
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+        }
