0a7d3b84c7ff01b4c23b90920a0f619d3ea1045a
ejabu <muhammadfahreza@gmail.com>
2017-01-18 11:55:16 +0700

    Mail Override

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ import vocab_week
 import vocab_quiz
 import student
 import vocab_task
+import mail
---
+++ models/mail.py
@@ -0,0 +1,10 @@
+from openerp import models, fields, api, _
+from email.utils import formataddr
+
+class mail_message(models.Model):
+    _inherit= "mail.message"
+
+    @api.v7
+    def _get_default_from(self, cr, uid, context=None):
+        import ipdb; ipdb.set_trace()
+        return formataddr(('Logger', 'Logger@gmail.com'))
